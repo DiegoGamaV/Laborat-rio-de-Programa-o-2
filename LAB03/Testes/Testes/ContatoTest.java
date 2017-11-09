00ef7ca6c88d82dfd1233f85ea7c1147b68ab78c
Diego A. Gama <diego.gama@ccc.ufcg.edu.br>
2017-11-09 11:45:16 -0300

    Update ContatoTest.java

--- LAB03/Testes/Testes/ContatoTest.java
+++ LAB03/Testes/Testes/ContatoTest.java
@@ -1,13 +1,14 @@
 /**
  * 
  */
-package Testes;
+package testes;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-import Lab03.Contato;
+
+import lab03.Contato;
 
 /**
  * @author diegoag
@@ -32,6 +33,35 @@ public class ContatoTest {
 		String mensagem = "Esperando obter métodos iguais. contatoTest1 a partir do construtor, "
 				+ "contatoTest2 a partir de settes";
 		assertEquals(mensagem, contatoTest1, contatoTest2);
+	}
+
+	// Testes de Argumentos Nulos
+	
+	@Test(expected = NullPointerException.class)
+	public void testContatoNullNome() {
+		Contato contatoNull = new Contato(null, "Sobrenome", "(83) 99999-0000");
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testContatoNullSobrenome() {
+		Contato contatoNull = new Contato("Nome", null, "(83) 99999-0000");
+	}
+	
+	// Testes de Argumentos Inválidos
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testContatosNullNome() {
+		Contato contatoSpace = new Contato(" ", "Sobrenome", "(83) 99999-0000");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testContatosNullSobrenome() {
+		Contato contatoSpace = new Contato("Nome", " ", "(83) 99999-0000");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testContatosNullTelefone() {
+		Contato contatoSpace = new Contato("Nome", "Sobrenome", " ");
 	}
 	
 	@Test
