760eaca3f00fb5e292bc00cf335904a9f1b2e974
Diego A. Gama <diego.gama@ccc.ufcg.edu.br>
2017-11-09 11:45:35 -0300

    Update Agenda.java

--- LAB03/src/Lab03/Agenda.java
+++ LAB03/src/Lab03/Agenda.java
@@ -1,31 +1,29 @@
-package Lab03;
+package lab03;
 
 public class Agenda {
-	
+
 	private Contato[] contatos;
-	
+
 	public Agenda() {
 		this.contatos = new Contato[100];
 	}
-	
+
 	public boolean setContato(String nome, String sobrenome, String telefone, int posicao) {
 		Contato contato = new Contato(nome, sobrenome, telefone);
 		posicao--;
-		if (posicao <= -1 || posicao >= 101){
+		if (posicao <= -1 || posicao >= 101) {
 			return false;
-		}
-		else {
+		} else {
 			this.contatos[posicao] = contato;
 			return true;
 		}
 	}
-	
+
 	public String getContato(int posicao) {
 		return this.contatos[posicao].toString();
 	}
-	
+
 	public String toString() {
 		return contatos.toString();
 	}
-	
 }
