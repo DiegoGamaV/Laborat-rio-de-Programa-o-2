1c26a3d2b502348910e52105271d30507cfab6e2
Diego A. Gama <diego.gama@ccc.ufcg.edu.br>
2017-11-30 11:44:15 -0300

    Update Aluno.java

--- LAB04/src/classes/Aluno.java
+++ LAB04/src/classes/Aluno.java
@@ -1,28 +1,43 @@
 package classes;
 
+/**
+ * @author Diego Gama 
+ * Classe utilizada para representar um aluno no sistema.
+ */
 public class Aluno {
 
 	private String matricula;
 	private String nome;
 	private String curso;
-	
+
 	public Aluno(String matricula, String nome, String curso) {
-		if (!isValido()) {
+		if (!isValido(matricula, nome, curso)) {
 			throw new IllegalArgumentException();
 		}
 		this.matricula = matricula;
 		this.nome = nome;
 		this.curso = curso;
 	}
-	
+
 	public String getMatricula() {
 		return this.matricula;
 	}
-	
-	public boolean isValido() {
-		if (this.matricula.isEmpty() ||this.nome.isEmpty() || this.curso.isEmpty()) {
+
+	/**
+	 * @method Método que valida se os argumentos são vazios ou nulos.
+	 * 
+	 * @param matricula
+	 *            matricula a ser testada
+	 * @param nome
+	 *            nome a ser testado
+	 * @param curso
+	 *            curso a ser testado
+	 * @return a validade dos argumentos.
+	 */
+	public boolean isValido(String matricula, String nome, String curso) {
+		if (matricula == null || nome == null || curso == null) {
 			return false;
-		} else if (this.matricula == null || this.nome == null || this.curso == null){
+		} else if (matricula.isEmpty() || nome.isEmpty() || curso.isEmpty()) {
 			return false;
 		} else {
 			return true;
@@ -53,9 +68,9 @@ public class Aluno {
 			return false;
 		return true;
 	}
-	
+
 	public String toString() {
-		return this.matricula + " - " + this.nome + " - " + this.curso;
+		return this.matricula.trim() + " - " + this.nome.trim() + " - " + this.curso.trim();
 	}
-	
+
 }
